7ef40a3be07b5ae470c35685758ff500959db86d
pxeldis <annettgerlach@gmx.net>
2020-08-20 00:21:13 +0200

    interpolation about minutes

--- farbverlauf.py
+++ farbverlauf.py
@@ -6,25 +6,45 @@ import time
 def aktuelle_h():
     return int(time.strftime("%H"))
 
-def time2color(h, min):
-    colors = ["yellow", "orange", "red", "purple", "blue", "green"]
-    return (4*colors)[h]
+def aktuelle_m():
+    return int(time.strftime("%M"))
 
-    r = "99"
-    g = "44"
-    b = "44"
-    return "#" + r + g + b
+def int2hex(i):
+    return "{:02x}".format(i)
+
+def hex2int(h):
+    return int(h,16)
+
+def interpolate_1colorchanel(start, ziel, amount):
+    result = amount * hex2int(ziel) + (1-amount) * hex2int(start)
+    return int2hex(min(255, int(result)))
+
+def interpolate_color(start, ziel, amount):
+    return interpolate_1colorchanel(start[0:2], ziel[0:2], amount) + \
+           interpolate_1colorchanel(start[2:4], ziel[2:4], amount) + \
+           interpolate_1colorchanel(start[4:6], ziel[4:6], amount) 
+
+def hour2color(h):
+    colors = ["87ff00", "3232ff", "9933ff", "ff4455", "ffaa55", "ffff00"]
+    return (4*colors)[h%24]
+
+def time2color(h, m):
+    return interpolate_color(hour2color(h), hour2color(h+1), m/60.0)
 
 def divForEachHour():
-    return " ".join(['<div style="width:30px; height:23px; background-color:' + time2color(h,0) + '"></div>'
+    return "\n    ".join(['<div style="width:60px; height:23px; background-color:#' + time2color(h,0) + '"></div>'
                      for h in range(24)])
 
+def divForEachMinute():
+    return "\n    ".join(['<div style="width:60px; height:10px; background-color:#' + time2color(aktuelle_h(), m) + '"></div>'
+                     for m in range(60)])
+
 def writeHtml():
-     aktuelle_farbe = time2color(aktuelle_h(),0)
+     aktuelle_farbe = time2color(aktuelle_h(),aktuelle_m())
      content="""
      <html>
-      <body style="background-color: """ + aktuelle_farbe + """">
-       """ + divForEachHour() + """
+      <body style="background-color: #""" + aktuelle_farbe + """">
+       """ + divForEachHour() + divForEachMinute() + """
       </body>
      </html>
      """
